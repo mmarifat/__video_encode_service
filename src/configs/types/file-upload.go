9efc67c0136e1e191f49bf759b39d7301c90d245
mmarifat <mma.rifat66@gmail.com>
2022-12-07 12:03:59 +0000

    - remove default mountPath

--- src/configs/types/file-upload.go
+++ src/configs/types/file-upload.go
@@ -3,13 +3,13 @@ package types
 import "mime/multipart"
 
 type FileJson struct {
-	MountPath *string               `form:"mountPath" binding:"required" default:"/mnt/streamstudio"`
+	MountPath *string               `form:"mountPath" binding:"required"`
 	Name      *string               `form:"name"`
 	File      *multipart.FileHeader `form:"file" binding:"required" swaggerignore:"true"`
 }
 
 type FileCompressJson struct {
-	MountPath     *string               `form:"mountPath" binding:"required" default:"/mnt/streamstudio"`
+	MountPath     *string               `form:"mountPath" binding:"required"`
 	EncodeWaiting *bool                 `form:"encodeWaiting" binding:"required" default:"false"`
 	Name          *string               `form:"name"`
 	FfmpegStr     *string               `form:"ffmpegStr" default:"-filter:v fps=25 -vf scale=1280:720 -b:v 880k -b:a 128k -c:v h264 -c:a aac -ac 2 -ar 44100"`
